1f0f5b8a736816dc4479423b5cd31917db7e4d51
Laurent <llebrun@talentsoft.com>
2017-12-06 17:00:30 +0100

    begin problem 10

--- Euler/Euler/NaturalNumbers.cs
+++ Euler/Euler/NaturalNumbers.cs
@@ -46,22 +46,11 @@ namespace Euler
             while (actualValue < 4000000)
             {
                 iteration++;
-                actualValue = Fibonacci(iteration);
+                actualValue = Utils.Fibonacci(iteration);
                 if (actualValue % 2 == 0 && actualValue < 4000000) { result += actualValue; }
             }
 
             return result;
-        }
-
-        public static long Fibonacci(int iteration)
-        {
-            if (iteration <= 0) { return 0; }
-            if (iteration == 1) { return 1; }
-            if (iteration == 2) { return 2; }
-            return Fibonacci(iteration - 2) + Fibonacci(iteration - 1);
-
-            //round( Phi^n / √5 ) Phi = (1+√5) / 2
-
         }
 
         /*
@@ -75,31 +64,9 @@ namespace Euler
 
         public static long LargestPrimeFactorOf(long numberToFactorize)
         {
-            var allPrimesFactors = PrimesFactorsOf(numberToFactorize);
+            var allPrimesFactors = Utils.PrimesFactorsOf(numberToFactorize);
             Array.Sort(allPrimesFactors);
             return allPrimesFactors[allPrimesFactors.Length - 1];
-        }
-
-        [NotNull]
-        public static long[] PrimesFactorsOf(long numberToFactorize)
-        {
-            var factors = new HashSet<long>();
-            var sieve = 2;
-            while (numberToFactorize > 1)
-            {
-                while (numberToFactorize % sieve == 0)
-                {
-                    factors.Add(sieve);
-                    numberToFactorize /= sieve;
-                }
-                sieve = sieve + 1;
-                if (sieve * sieve <= numberToFactorize) { continue; }
-                if (numberToFactorize > 1) { factors.Add(numberToFactorize); }
-            }
-
-            var result = new long[factors.Count];
-            factors.CopyTo(result);
-            return result;
         }
 
         /*
@@ -137,22 +104,8 @@ namespace Euler
         {
             string challenger = candidate.ToString();
             string champion = older.ToString();
-            if (!IsAPalindrom(challenger)) { return false; }
+            if (!Utils.IsAPalindrom(challenger)) { return false; }
             return challenger.Length > champion.Length;
-        }
-
-        public static bool IsAPalindrom([NotNull] string toEvaluate)
-        {
-            if (toEvaluate.Length % 2 != 0) { return false; }
-            string left = toEvaluate.Substring(0, toEvaluate.Length / 2);
-
-            string futureRight = toEvaluate.Substring(toEvaluate.Length / 2);
-            char[] right = new char[futureRight.Length];
-            futureRight.CopyTo(0, right, 0, futureRight.Length);
-            Array.Reverse(right);
-
-            for (int index = 0; index < left.Length; index++) { if (left[index] != right[index]) { return false; } }
-            return true;
         }
 
         /*
@@ -168,7 +121,7 @@ namespace Euler
             bool looping = true;
             while (looping)
             {
-                if (IsEvenlyDivisibleBy(candidate, factors))
+                if (Utils.IsEvenlyDivisibleBy(candidate, factors))
                 {
                     result = candidate;
                     looping = false;
@@ -177,12 +130,6 @@ namespace Euler
             }
 
             return result;
-        }
-
-        public static bool IsEvenlyDivisibleBy(int candidate, [NotNull] IEnumerable<int> factors)
-        {
-            foreach (var oneFactor in factors) { if (candidate % oneFactor != 0) { return false; } }
-            return true;
         }
 
         /*
@@ -219,67 +166,15 @@ namespace Euler
             var tenThousandPrimes = new[] { 1UL };
             ulong limit = (ulong)Math.Pow(2, 16);
 
-            tenThousandPrimes = FindPrimes(limit, tenThousandPrimes);
+            tenThousandPrimes = Utils.FindPrimes(limit, tenThousandPrimes);
             while (tenThousandPrimes.Length < 10001)
             {
-                tenThousandPrimes = FindPrimes(limit, tenThousandPrimes);
+                tenThousandPrimes = Utils.FindPrimes(limit, tenThousandPrimes);
                 limit *= 2;
             }
 
             ulong result = tenThousandPrimes[10001];
             return result;
-        }
-
-        [NotNull]
-        private static unsafe ulong[] FindPrimes(ulong limit, [NotNull] ulong[] alreadyKnownPrimes)
-        {
-            var tampon = new List<ulong>();
-            var isPrime = new bool[limit + 1];
-            var lowerLimit = Math.Sqrt(limit);
-            ulong largestValue = alreadyKnownPrimes[alreadyKnownPrimes.Length - 1] > 0
-                ? alreadyKnownPrimes[alreadyKnownPrimes.Length - 1]
-                : 1;
-            Array.Sort(alreadyKnownPrimes);
-
-            fixed (bool* pp = isPrime)
-            {
-                bool* pp1 = pp;
-                tampon.AddRange(Array.FindAll(alreadyKnownPrimes, item => item <= limit));
-                if (limit <= largestValue) { return tampon.ToArray(); }
-                for (ulong x = 1; x <= lowerLimit; x++)
-                    for (ulong y = 1; y <= lowerLimit; y++)
-                    {
-                        var n = 4 * x * x + y * y;
-                        if (n <= limit && (n % 12 == 1 || n % 12 == 5)) pp1[n] ^= true;
-
-                        n = 3 * x * x + y * y;
-                        if (n <= limit && n % 12 == 7) pp1[n] ^= true;
-
-                        n = 3 * x * x - y * y;
-                        if (x > y && n <= limit && n % 12 == 11) pp1[n] ^= true;
-                    }
-
-                for (ulong n = 5; n <= lowerLimit; n++)
-                    if (pp1[n])
-                    {
-                        var s = n * n;
-                        for (ulong k = s; k <= limit; k += s) pp1[k] = false;
-                    }
-
-                if (largestValue < 3)
-                {
-                    tampon.Add(2);
-                    tampon.Add(3);
-                }
-                for (ulong n = 5; n <= limit; n += 2) if (pp1[n]) tampon.Add(n);
-            }
-
-
-            ulong[] sortie = new ulong[tampon.Count];
-            new HashSet<ulong>(tampon).CopyTo(sortie);
-            Array.Sort(sortie);
-            sortie = Array.FindAll(sortie, item => item != 0);
-            return sortie;
         }
 
         /*
@@ -375,20 +270,79 @@ namespace Euler
                     {
                         if (!(prem < deux && deux < trio)) { continue; }
                         if (prem + deux + trio != 1000) { continue; }
-                        if (IsAPythagoreanTriplet(prem, deux, trio)) { return prem * deux * trio; }
+                        if (Utils.IsAPythagoreanTriplet(prem, deux, trio)) { return prem * deux * trio; }
                     }
                 }
             }
             return 0;
         }
 
-        public static bool IsAPythagoreanTriplet(int prem, int deux, int trio)
+        /*
+         * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+         * Find the sum of all the primes below two million.
+         */
+        public static long SummationOfPrimes()
         {
-            var first = prem * prem;
-            var second = deux * deux;
-            var third = trio * trio;
-            if (first + second == third) { return true; }
-            return false;
+            ulong result = 0;
+
+            var candidates = Utils.FindPrimes(2000000, new ulong[] { 0 });
+            Array.ForEach(candidates, prime => result += prime);
+
+            return (long)result;
+        }
+
+        /*
+         * In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+         *          08,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,08
+         *          49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00
+         *          81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65
+         *          52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91
+         *          22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80
+         *          24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50
+         *          32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70
+         *          67,26,20,68,02,62,12,20,95,63,94,39,63,08,40,91,66,49,94,21
+         *          24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72
+         *          21,36,23,09,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95
+         *          78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14,09,53,56,92
+         *          16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57
+         *          86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58
+         *          19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40
+         *          04,52,08,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66
+         *          88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69
+         *          04,42,16,73,38,25,39,11,24,94,72,18,08,46,29,32,40,62,76,36
+         *          20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16
+         *          20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54
+         *          01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48
+         * The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+         * What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+         */
+        public static long LargestProductInGrid()
+        {
+            var grid = new int[,]
+            {
+                {08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08},
+                {49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00},
+                {81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65},
+                {52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91},
+                {22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80},
+                {24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50},
+                {32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70},
+                {67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21},
+                {24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72},
+                {21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95},
+                {78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92},
+                {16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57},
+                {86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58},
+                {19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40},
+                {04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66},
+                {88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69},
+                {04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36},
+                {20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16},
+                {20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54},
+                {01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48}
+            };
+
+
         }
     }
 
---
+++ Euler/Euler/Utils.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Euler
+{
+    internal static class Utils
+    {
+        public static long Fibonacci(int iteration)
+        {
+            if (iteration <= 0) { return 0; }
+            if (iteration == 1) { return 1; }
+            if (iteration == 2) { return 2; }
+            return Fibonacci(iteration - 2) + Fibonacci(iteration - 1);
+
+            //round( Phi^n / √5 ) Phi = (1+√5) / 2
+
+        }
+
+        [NotNull]
+        public static long[] PrimesFactorsOf(long numberToFactorize)
+        {
+            var factors = new HashSet<long>();
+            var sieve = 2;
+            while (numberToFactorize > 1)
+            {
+                while (numberToFactorize % sieve == 0)
+                {
+                    factors.Add(sieve);
+                    numberToFactorize /= sieve;
+                }
+                sieve = sieve + 1;
+                if (sieve * sieve <= numberToFactorize) { continue; }
+                if (numberToFactorize > 1) { factors.Add(numberToFactorize); }
+            }
+
+            var result = new long[factors.Count];
+            factors.CopyTo(result);
+            return result;
+        }
+
+        public static bool IsAPalindrom([NotNull] string toEvaluate)
+        {
+            if (toEvaluate.Length % 2 != 0) { return false; }
+            string left = toEvaluate.Substring(0, toEvaluate.Length / 2);
+
+            string futureRight = toEvaluate.Substring(toEvaluate.Length / 2);
+            char[] right = new char[futureRight.Length];
+            futureRight.CopyTo(0, right, 0, futureRight.Length);
+            Array.Reverse(right);
+
+            for (int index = 0; index < left.Length; index++) { if (left[index] != right[index]) { return false; } }
+            return true;
+        }
+
+        public static bool IsEvenlyDivisibleBy(int candidate, [NotNull] IEnumerable<int> factors)
+        {
+            foreach (var oneFactor in factors) { if (candidate % oneFactor != 0) { return false; } }
+            return true;
+        }
+
+        [NotNull]
+        public static unsafe ulong[] FindPrimes(ulong limit, [NotNull] ulong[] alreadyKnownPrimes)
+        {
+            var tampon = new List<ulong>();
+            var isPrime = new bool[limit + 1];
+            var lowerLimit = Math.Sqrt(limit);
+            ulong largestValue = Math.Max(alreadyKnownPrimes[alreadyKnownPrimes.Length - 1], 1);
+
+            Array.Sort(alreadyKnownPrimes);
+
+            fixed (bool* pp = isPrime)
+            {
+                bool* pp1 = pp;
+                tampon.AddRange(Array.FindAll(alreadyKnownPrimes, item => item <= limit));
+                if (limit <= largestValue) { return tampon.ToArray(); }
+                for (ulong x = 1; x <= lowerLimit; x++)
+                for (ulong y = 1; y <= lowerLimit; y++)
+                {
+                    var n = 4 * x * x + y * y;
+                    if (n <= limit && (n % 12 == 1 || n % 12 == 5)) pp1[n] ^= true;
+
+                    n = 3 * x * x + y * y;
+                    if (n <= limit && n % 12 == 7) pp1[n] ^= true;
+
+                    n = 3 * x * x - y * y;
+                    if (x > y && n <= limit && n % 12 == 11) pp1[n] ^= true;
+                }
+
+                for (ulong n = 5; n <= lowerLimit; n++)
+                    if (pp1[n])
+                    {
+                        var s = n * n;
+                        for (ulong k = s; k <= limit; k += s) pp1[k] = false;
+                    }
+
+                if (largestValue < 3)
+                {
+                    tampon.Add(2);
+                    tampon.Add(3);
+                }
+                for (ulong n = 5; n <= limit; n += 2) if (pp1[n]) tampon.Add(n);
+            }
+
+
+            ulong[] sortie = new ulong[tampon.Count];
+            new HashSet<ulong>(tampon).CopyTo(sortie);
+            Array.Sort(sortie);
+            sortie = Array.FindAll(sortie, item => item != 0);
+            return sortie;
+        }
+
+        public static bool IsAPythagoreanTriplet(int prem, int deux, int trio)
+        {
+            var first = prem * prem;
+            var second = deux * deux;
+            var third = trio * trio;
+            if (first + second == third) { return true; }
+            return false;
+        }
+    }
+}
